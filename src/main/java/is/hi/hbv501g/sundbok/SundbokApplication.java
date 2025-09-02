64b71a2a154388a47e43ded004737c3aca048d79
Admentus-Dragon <mikki5564@gmail.com>
2025-09-02 17:44:20 +0000

    Commit comment

--- src/main/java/is/hi/hbv501g/sundbok/SundbokApplication.java
+++ src/main/java/is/hi/hbv501g/sundbok/SundbokApplication.java
@@ -1,13 +1,15 @@
 package is.hi.hbv501g.sundbok;
 // Arnar Simonsen was here
+// Mikael was not here I was there
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SundbokApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SundbokApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SundbokApplication.class, args);
+    }
 
 }
