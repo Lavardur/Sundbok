262e7001ccf56a09eea6c5c00077f4bff1cae373
Lavardur <antonben44@gmail.com>
2025-09-02 14:21:33 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/sundbok/SundbokApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.sundbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundbokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundbokApplication.class, args);
+	}
+
+}
