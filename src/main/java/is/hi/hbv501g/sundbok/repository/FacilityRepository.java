b395c5bdc61da910ba1602052f52693086c8fc7d
Sævar Axel Bjarnason <saevaraxel123@gmail.com>
2025-10-15 19:14:41 +0000

    Review virkni bætt við (controller og service)

--- src/main/java/is/hi/hbv501g/sundbok/repository/FacilityRepository.java
+++ src/main/java/is/hi/hbv501g/sundbok/repository/FacilityRepository.java
@@ -1,8 +1,12 @@
 package is.hi.hbv501g.sundbok.repository;
 
 import is.hi.hbv501g.sundbok.model.Facility;
+import is.hi.hbv501g.sundbok.model.Review;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface FacilityRepository extends CrudRepository<Facility, Long> {
     // You can add custom query methods here if needed
+        List<Review> findByFacilityId(Long facilityId);
 }
